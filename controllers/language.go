26ebd7204d20a1b8a3f35e15ee26fa9bad6009ad
Francisco Barahona <francisco.barahona.sarmiento@gmail.com>
2020-05-31 00:27:41 -0500

    Enhancements to 'FindAll' endpoint.

--- controllers/language.go
+++ controllers/language.go
@@ -1,22 +1,43 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/fsunset/goBasicAPI/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // FindAll returns all languages in DB
 func FindAll(ctx *gin.Context) {
-	// Initialize variable to store query-results. Must have correct structure
-	var languages []models.Language
+	collection := models.MongoConnection.Database("languagesDB").Collection("language")
+
+	// Initialize slice to store ALL collection's documents returned by query. Must have correct "Language" structure
+	var allLanguages []models.Language
 
 	// Quering...
-	models.MongoConnection.Database("languagesDB").Collection("languages").Find(ctx, &languages)
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Prepare data to be returned...
+	for cur.Next(ctx) {
+		// Initialize variable to store each document. Must have correct "Language" structure
+		var language models.Language
+
+		err := cur.Decode(&language)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Appending each document within "allLanguages" slice
+		allLanguages = append(allLanguages, language)
+	}
 
 	// Returning data
 	ctx.JSON(http.StatusOK, gin.H{
-		"results": languages,
+		"results": allLanguages,
 	})
 }
