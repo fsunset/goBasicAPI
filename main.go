b9be73bd89d9ff21eeaf10d684e62a94d1e1a7ff
Francisco Barahona <francisco.barahona.sarmiento@gmail.com>
2020-05-30 21:55:52 -0500

    Boilerplate.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"response": "Hello World!"})
+	})
+
+	server.Run()
+}
